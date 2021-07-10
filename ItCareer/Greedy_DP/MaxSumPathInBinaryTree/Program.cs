a1d2da5944b851893cc26edca34b04511b2b2375
Victor Stamoff <transtrike@gmail.com>
2021-07-10 23:38:09 +0300

    Formatting

--- ItCareer/Greedy_DP/MaxSumPathInBinaryTree/Program.cs
+++ ItCareer/Greedy_DP/MaxSumPathInBinaryTree/Program.cs
@@ -2,44 +2,45 @@
 
 namespace MaxSumPathInBinaryTree
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            BinaryTree<int> binaryTree = new BinaryTree<int>(7,
-                new BinaryTree<int>(19,
-                    new BinaryTree<int>(3),
-                    new BinaryTree<int>(12)),
-                new BinaryTree<int>(14,
-                    new BinaryTree<int>(23),
-                    new BinaryTree<int>(5)));
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			BinaryTree<int> binaryTree = new BinaryTree<int>(7,
+				new BinaryTree<int>(19,
+					new BinaryTree<int>(3),
+					new BinaryTree<int>(12)),
+				new BinaryTree<int>(14,
+					new BinaryTree<int>(23),
+					new BinaryTree<int>(5)));
 
-            Greedy(binaryTree);
-        }
+			Greedy(binaryTree);
+		}
 
-        static void Greedy(BinaryTree<int> tree)
-        {
-            if (IsLeaf(tree))
-            {
-                return;
-            }
-            else if (tree.LeftNode.Value >= tree.RightNode.Value)
-            {
-                System.Console.WriteLine(tree.LeftNode.Value);
-                Greedy(tree.LeftNode);
-            }
-            else
-            {
-                System.Console.WriteLine(tree.RightNode.Value);
-                Greedy(tree.RightNode);
-            }
-        }
+		static void Greedy(BinaryTree<int> tree)
+		{
+			if (IsLeaf(tree))
+			{
+				return;
+			}
+			else if (tree.LeftNode.Value >= tree.RightNode.Value)
+			{
+				System.Console.WriteLine(tree.LeftNode.Value);
+				Greedy(tree.LeftNode);
+			}
+			else
+			{
+				System.Console.WriteLine(tree.RightNode.Value);
+				Greedy(tree.RightNode);
+			}
+		}
 
-        static bool IsLeaf(BinaryTree<int> tree)
-        {
-            if (tree.LeftNode == null && tree.RightNode == null)
-                return true;
-            else
-                return false;
-        }
-    }
+		static bool IsLeaf(BinaryTree<int> tree)
+		{
+			if (tree.LeftNode == null && tree.RightNode == null)
+				return true;
+			else
+				return false;
+		}
+	}
+}
--- ItCareer/Warmup/AVeryBigSum/Program.cs
+++ ItCareer/Warmup/AVeryBigSum/Program.cs
@@ -2,35 +2,34 @@
 
 namespace AVeryBigSum
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            int n = int.Parse(Console.ReadLine());
-            string[] input = Console.ReadLine().Split(' ');
-            long[] arr = ParseToLongArr(input);
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int n = int.Parse(Console.ReadLine());
+			string[] input = Console.ReadLine().Split(' ');
+			long[] arr = ParseToLongArr(input);
 
-            LongSum(arr);
-        }
+			LongSum(arr);
+		}
 
-        static void LongSum(long[] array)
-        {
-            long sum = 0;
-            for (int i = 0; i < array.Length; i++)
-                sum += array[i];
+		static void LongSum(long[] array)
+		{
+			long sum = 0;
+			for (int i = 0; i < array.Length; i++)
+				sum += array[i];
 
-            System.Console.Write(sum);
+			System.Console.Write(sum);
 
-            System.
-        }
+		}
 
-        static long[] ParseToLongArr(string[] array)
-        {
-            long[] arr = new long[array.Length];
-            for (int i = 0; i < array.Length; i++)
-                arr[i] = long.Parse(array[i]);
+		static long[] ParseToLongArr(string[] array)
+		{
+			long[] arr = new long[array.Length];
+			for (int i = 0; i < array.Length; i++)
+				arr[i] = long.Parse(array[i]);
 
-            return arr;
-        } 
-    }
+			return arr;
+		}
+	}
 }
